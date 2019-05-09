0f4cd9b05cb5c0ade274e314be11738c61eac59f
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-05-09 17:03:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,6 @@
+require 'pry'
 class Song
-
-  attr_accessor :name, :artist, :genre
-
+  attr_accessor :name, :artist
   @@all = []
 
   def initialize(name)
@@ -9,12 +8,17 @@ class Song
     @@all << self
   end
 
-  def artist_name
-    self.artist == nil ? nil : self.artist.name
+  def self.new_by_filename(file_name)
+    artist_name = file_name.split(" - ")[0]
+    song_name = file_name.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist = Artist.find_or_create_by_name(artist_name)
+    song.artist.add_song(song)
+    song
   end
 
   def self.all
-   @@all
+    @@all
   end
 
 end
